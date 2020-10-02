6d76c4300bf13b0763fdfe78ca347b250bdc6acd
rishabh-husky <rishabhsethi29@gmail.com>
2020-10-02 21:33:40 +0530

    Improve LSP Rectangle Spec

--- src/SOLID/lsp/__tests__/Rectangle.spec.ts
+++ src/SOLID/lsp/__tests__/Rectangle.spec.ts
@@ -16,6 +16,10 @@ describe("Liskov Substitution Principle", () => {
 
   it("calculate area of rectangle and square", () => {
     const rect: Rectangle = new Rectangle(10, 20);
+    expect(rect)
+      .to.be.an("Object")
+      .that.is.an.instanceOf(Rectangle)
+      .deep.includes({ width: 10, height: 20 });
     const rectArea = useIt(rect);
     expect(rectArea).to.be.a("number").equal(100);
     /**
@@ -23,6 +27,11 @@ describe("Liskov Substitution Principle", () => {
      * since, setHeight changes both height and width to 10 ( 10 * 10 = 100)
      */
     const square: Square = new Square(5);
+    expect(square)
+      .to.be.an("Object")
+      .that.is.an.instanceOf(Square)
+      .that.is.an.instanceOf(Rectangle)
+      .deep.includes({ width: 5, height: 5 });
     const squareArea = useIt(square);
     expect(squareArea).to.be.a("number").equal(100).to.not.equal(50);
   });
